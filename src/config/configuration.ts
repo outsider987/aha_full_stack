09fd5a35bfc4d129e3a462f11b0e3f99861a1563
MediconcenVictor <victor_chang@mediconcen.com>
2023-09-26 21:36:59 +0800

    align code

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -6,17 +6,17 @@ export default () => ({
   jwt: {
     secret: process.env.JWT_SECRET,
     expiresIn: process.env.JWT_EXPIRATION_TIME,
-    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRATION_TIME,
+    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRATION_TIME
   },
   defaultConnection: {
-    type: "mysql",
+    type: 'mysql',
     replication: {
       master: {
         host: process.env.DB_HOST,
         port: parseInt(process.env.DB_PORT),
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        database: process.env.DB_NAME
       },
       slaves: [
         {
@@ -24,19 +24,19 @@ export default () => ({
           port: parseInt(process.env.DB_PORT),
           username: process.env.DB_USER,
           password: process.env.DB_PASSWORD,
-          database: process.env.DB_NAME,
-        },
-      ],
+          database: process.env.DB_NAME
+        }
+      ]
     },
 
-    migrations: ["dist/migrations/*.js"],
-    entities: ["dist/entities/*.js"],
-    synchronize: false,
+    migrations: ['dist/migrations/*.js'],
+    entities: ['dist/entities/*.js'],
+    synchronize: false
   },
   gooleOauth: {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: `${process.env.BACKEND_ENDPOINT}/auth/google/callback`,
-    scope: ["openid", "email", "profile"],
-  },
+    scope: ['openid', 'email', 'profile']
+  }
 });
